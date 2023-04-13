ce959e9a4586361d3b7c087bc702e14552381a75
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-13 16:46:05 +0530

    Refactor: Write JUnit Parameterised Test to validate multiple entry for the Email Address

--- Testing.java
+++ Testing.java
@@ -1,47 +1,33 @@
 package com.bridgelabz.userRegistration;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class Testing{
+    private final String email;
+    private final Boolean expectedResult;
+    public UserRegistration userRegistration;
+
+    public Testing(String email, Boolean expectedResult) {
+        this.email = email;
+        this.expectedResult = expectedResult;
+    }
     @Before
-    public  void printBeforeAllMsg() {
-        System.out.println("-------Welcome to the User Registration Program-------");
+    public void initilize(){
+    userRegistration = new UserRegistration();
     }
-    @After
-    public void PrintAfterEachTestCasePass(){
-        System.out.println("passed");
-    }
-    @Test
-    public void givenFirstNameShouldBeValid(){
-        UserRegistration user = new UserRegistration();
-        assertEquals("Valid FirstName", user.checkFirstName("Ramesh"));
-        System.out.println("Happy");
+    @Parameterized.Parameters
+    public static Collection input(){
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com",true},{"abc-100@yahoo.com",true},{"abc.100@yahoo.com",true},{"abc111@abc.com",true},{"abc-100@abc.net",true},{"abc.100@abc.com.au",true},{"abc@1.com",true},{"abc@gmail.com.com",true},{"abc+100@gmail.com",true}});
     }
     @Test
-    public void givenLastNameShouldBeValid(){
-        UserRegistration user = new UserRegistration();
-        assertEquals("Valid LastName", user.checkLastName("Ambati"));
-        System.out.println("Happy");
-    }
-    @Test
-    public void givenEmailShouldBeValid(){
-        UserRegistration user = new UserRegistration();
-        assertEquals("Valid Email", user.checkEmailId("rameshambati.ab@gmail.com"));
-        System.out.println("Happy");
-    }
-    @Test
-    public void givenPhoneNumberShouldBeValid(){
-        UserRegistration user = new UserRegistration();
-        assertEquals("Valid PhoneNumber", user.phoneNumber("91 9640827483"));
-        System.out.println("Happy");
-    }
-    @Test
-    public void givenPasswordShouldBeValid(){
-        UserRegistration user = new UserRegistration();
-        assertEquals("Valid Password", user.password("Rohitsharma45@"));
-        System.out.println("Happy");
+    public void testEmailValidation(){
+        System.out.println(email+ "======== is valid");
+        assertEquals(expectedResult,userRegistration.checkEmail(email));
     }
 }
--- UserRegistration.java
+++ UserRegistration.java
@@ -3,43 +3,9 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    Scanner scanner = new Scanner(System.in);
-    public static final String VALIDATE_NAME="^[A-Z]+[a-z]{2,}$";
-    public static final String EMAIL_ID="^[A-Za-z]{3,}[.-]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
-    public static final String PHONE_NUMBER="^[0-9]+\s[0-9]{10}$";
-    public static final String PASSWORD="^[A-Z]+[a-z0-9]{7,}[!@#$%&*]+$";
-    public String checkFirstName(String firstName)  {
-        if (Pattern.matches(VALIDATE_NAME, firstName))
-            return "Valid FirstName";
-        else
-            return "Invalid FirstName , Please Enter Valid FirstName";
-    }
-
-    public String checkLastName(String lastName)  {
-        if (Pattern.matches(VALIDATE_NAME,lastName))
-            return "Valid LastName";
-        else
-            return "Invalid Last Name , Please Enter Valid LastName";
-    }
-
-    public String checkEmailId(String emailId) {
-        if (Pattern.matches(EMAIL_ID,emailId))
-            return "Valid Email";
-        else
-           return  "Invalid Email ID, Please Enter Valid Email ID";
-    }
-
-    public String phoneNumber(String phoneNumber) {
-        if (Pattern.matches(PHONE_NUMBER,phoneNumber))
-            return "Valid PhoneNumber";
-        else
-           return  "Invalid PhoneNumber, Please Enter Valid PhoneNumber";
-    }
-
-    public String password(String password) {
-        if (Pattern.matches(PASSWORD,password))
-            return "Valid Password";
-        else
-           return  "Invalid Password, Please Enter Valid Password";
+    public static final String SEPARATE_EMAIL_ID="^[A-Za-z]{3,}[.+-]?[A-Za-z0-9]*[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,}[.]?[a-z]*$";
+    public Boolean checkEmail(String email){
+        Boolean expectedResult=(Pattern.matches(SEPARATE_EMAIL_ID,email));
+        return expectedResult;
     }
 }
